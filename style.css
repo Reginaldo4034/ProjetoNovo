13886307c5f3dc2e36564b92d8fb1f97722c67c2
Reginaldo4034 <86808073+Reginaldo4034@users.noreply.github.com>
2021-08-21 19:03:32 -0300

    Update style.css
    Utilizando react através do css

--- style.css
+++ style.css
@@ -1,1 +1,42 @@
+react/components/  <component_name>
+export default ProductPrice
+import styleModule from './style.module.css'
 
+import ProductPrice from './components/ProductPrice/index'
+/* /react/MyButton.tsx */
+import styles from './styles.css'
+
+export default function MyButton() {
+  return (
+    <button className={styles.myButton}>
+      My button
+    </button>
+  )
+}
+const lista = () => (
+  <div className="flex justify-center pv4 ph3 bg-base--inverted">
+    <p>Boa tarde sejam   bem-vindos</p>
+  </div>
+)
+import { useCssHandles } from 'vtex.css-handles'
+
+const CSS_HANDLES = ['container', 'background', 'text', 'item'] as const
+/* Using `as const at the end` hints to Typescript that this array
+ * won't change, thus allowing autocomplete and other goodies. */
+
+const Component: FC = () => {
+  const handles = useCssHandles(CSS_HANDLES)  
+
+  <div className={handles.container}>
+  <div className={`${handles.background} bg-Black`}>
+    <h1 className={`${handles.text} f1 c-yellow`}> Upmedal.com </h1>
+    <ul>
+      {['blue', 'yellow', 'green'].map(color => (
+        <li
+          className={`${handles.item} bg-${color::first-letter}`}>
+          Color {color:blue}
+        </li>
+      ))}
+    </ul>
+  </div>
+</div>
